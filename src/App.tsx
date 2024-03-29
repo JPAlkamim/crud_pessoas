ad9e3acf9f4386c640b6272ecb6544c5903a434d
JPAlkamim <jpalkamim@gmail.com>
2024-03-29 12:51:32 -0300

    Preparando ambiente

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1>Cadastro de Pessoas</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
